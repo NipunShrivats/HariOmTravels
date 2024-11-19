b8da2937af2e530ad531690a77788f71e7709dae
Nipun Shrivats <nipunrawat16@gmail.com>
2024-11-19 21:12:24 +0530

    changes on Nov19 commit no.1

--- Frontend/src/Pages/TourDetails/TourDetails.jsx
+++ Frontend/src/Pages/TourDetails/TourDetails.jsx
@@ -1,5 +1,33 @@
 import React from "react";
+import "./TourDetails.css";
+import { Container, Row, Col, Form, ListGroup } from "reactstrap";
+import { useParams } from "react-router-dom";
+import tourData from "../../assets/data/tours";
 
 export default function TourDetails() {
-  return <div>TourDetails</div>;
+  const { id } = useParams();
+
+  // static for now
+  const tour = tourData.find((tour) => tour.id === id);
+
+  const { photo, title, desc, price, reviews, city, distance, maxGroupSize } =
+    tour;
+  return (
+    <>
+      <section>
+        <Container>
+          <Row>
+            <Col lg="8">
+              <div className="tour_content">
+                <img src={photo} alt="" />
+                <div className="tour_info">
+                  <h2>{title}</h2>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </>
+  );
 }
