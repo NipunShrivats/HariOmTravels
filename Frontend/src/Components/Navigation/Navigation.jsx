d9db8c8c619f46bcc8dddcc30032c77f20db248c
Nipun Shrivats <nipunrawat16@gmail.com>
2024-12-13 20:32:44 +0530

    changes on Nov26 commit no.2

--- Frontend/src/Components/Navigation/Navigation.jsx
+++ Frontend/src/Components/Navigation/Navigation.jsx
@@ -3,6 +3,8 @@ import "./Navigation.css";
 import logo from "../../assets/images/logo.png";
 import { Link } from "react-router-dom";
 export default function Navigation() {
+  const [showMediaIcon, setShowMediaIcon] = React.useState(false);
+  // console.log(showMediaIcon);
   return (
     <>
       <nav className="main-nav">
@@ -10,35 +12,44 @@ export default function Navigation() {
         <div className="logo">
           <img src={logo} alt="" />
         </div>
-        {/* menu right1 */}
-        <div className="menu-links-1">
-          <ul>
-            <Link to={"#"}>
-              <li>Home</li>
-            </Link>
-            <Link to={"#"}>
-              <li>About</li>
-            </Link>
-            <Link to={"#"}>
-              <li>Services</li>
-            </Link>
-            <Link to={"#"}>
-              <li>Contact</li>
-            </Link>
-          </ul>
+        <div
+          className={
+            showMediaIcon ? "navLink-box menu-links-mobile" : "navLink-box"
+          }
+        >
+          {/* menu right1 */}
+          <div className={"menu-links-1"}>
+            <ul>
+              <Link to={"#"}>
+                <li>Home</li>
+              </Link>
+              <Link to={"#"}>
+                <li>About</li>
+              </Link>
+              <Link to={"#"}>
+                <li>Services</li>
+              </Link>
+              <Link to={"#"}>
+                <li>Contact</li>
+              </Link>
+            </ul>
+          </div>
+          {/* menu right2 */}
+          <div className="menu-links-2">
+            <ul>
+              <Link to={"#"}>
+                <li>Login</li>
+              </Link>
+              <Link to={"#"}>
+                <li>Register</li>
+              </Link>
+            </ul>
+          </div>
         </div>
-        {/* menu right1 */}
-        <div className="menu-links-2">
-          <ul>
-            <Link to={"#"}>
-              <li>Login</li>
-            </Link>
-            <Link to={"#"}>
-              <li>Register</li>
-            </Link>
-          </ul>
-        </div>
-        <div className="hamburder-icon">
+        <div
+          className="hamburder-icon"
+          onClick={() => setShowMediaIcon(!showMediaIcon)}
+        >
           <span className="line">
             <i className="ri-menu-line"></i>
           </span>
